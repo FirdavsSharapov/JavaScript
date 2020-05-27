a9e37297c9a1667fbfd746818eeef67edbaa5da1
Fred Sharapov <f.sharapov@yahoo.com>
2020-05-26 22:23:50 -0400

    Final part of the clock. Ship it

--- JavaScript/JS_Clock/script.js
+++ JavaScript/JS_Clock/script.js
@@ -1,3 +1,22 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+function runClock() {
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let hrPosition = (hr * 360 / 12) + (hr * (360 / 60) / 12);
+    let minPostion = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = sec * 360 / 60;
+
+    // Set style for objects 
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPostion + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runClock, 1000);
+
