e637e9ec1f199df14271624441005a572598dcca
Fred Sharapov <f.sharapov@yahoo.com>
2020-07-21 10:17:50 -0400

    Changed index.js with more props

--- JavaScript/JS_React/02_02/react-is-fun/src/index.js
+++ JavaScript/JS_React/02_02/react-is-fun/src/index.js
@@ -23,7 +23,7 @@ class Message extends React.Component {
   render() {
     console.log(this.props)
     return (
-      <div style={{color: this.props.color}}>
+      <div style={{ color: this.props.color }}>
         <h1>{this.props.msg}</h1>
         <p>age = {this.props.age}</p>
       </div>
@@ -31,6 +31,35 @@ class Message extends React.Component {
   }
 }
 
+
+let skiData = {
+  total: 50,
+  posder: 20,
+  backcountry: 10,
+  goal: 100
+}
+
+class SkiDayCounter extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2>Ski Days</h2>
+        <div>
+          <p>Total Days: {this.props.total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {this.props.powder}</p>
+        </div>
+        <div>
+          <p>backcountry Days: {this.props.backcountry}</p>
+        </div>
+        <div>
+          <p>Goal: {this.props.goal}</p>
+        </div>
+      </section>
+    )
+  }
+}
 // var style = {
 //   backgroundColor: 'orange',
 //   color: 'white',
@@ -42,6 +71,12 @@ class Message extends React.Component {
 //   React.createElement('li', {}, 'item on our list'))
 
 ReactDOM.render(
-  <Message age={50} msg="how are you?" color ="blue"/>,
+  // <Message age={50} msg="how are you?" color ="blue"/>
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.backcountry}
+  />,
   document.getElementById('root')
 )
