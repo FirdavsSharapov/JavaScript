78b93b6f1e403d114f3247d0dc188091821abf88
Fred Sharapov <f.sharapov@yahoo.com>
2020-09-06 23:44:41 -0400

    styling was added

--- JavaScript/JS_React/todo_project/todo-app/src/components/TimePredictor.js
+++ JavaScript/JS_React/todo_project/todo-app/src/components/TimePredictor.js
@@ -3,22 +3,30 @@ import '../style/style.css';
 
 function TimePredictor() {
     const hours = new Date().getHours;
+    const styles = {
+        fontSize: 30,
+        backgroundColor: "#87ceeb",
+        textAlign: "center",
+    }
     let timeOfDay;
-
+    
     if (hours < 12) {
         timeOfDay = 'Morning';
+        styles.color = "green";
     }
     else if (hours >= 12 && hours < 17) {
         timeOfDay = 'Afternoon';
+        styles.color = "orange";
     }
     else {
         timeOfDay = 'Night';
+        styles.color = "Red";
     }
 
     return (
-        <div className="timepredictor">
-        <h1>Good {timeOfDay} </h1>
-        </div>
+        
+        <h1 style={styles}>Good {timeOfDay} </h1>
+
     );
 };
 
