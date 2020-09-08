bb0f4d6483a58a625e3c737e3a39830e3ac7b74d
Fred Sharapov <f.sharapov@yahoo.com>
2020-09-07 22:51:58 -0400

    class based react

---
+++ JavaScript/JS_React/todo_project/todo-app/src/components/ClassBasedComponents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ClassBasedComponents extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Code goes here!!!</h1>
+            </div>
+        );
+    };
+};
