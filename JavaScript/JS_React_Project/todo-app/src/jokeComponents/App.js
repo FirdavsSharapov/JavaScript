b454cb075b2ebf96eb94076fc35bb6102698b56c
Fred Sharapov <f.sharapov@yahoo.com>
2020-11-15 20:35:43 -0500

    Itterating through the JSON

--- JavaScript/JS_React_Project/todo-app/src/jokeComponents/App.js
+++ JavaScript/JS_React_Project/todo-app/src/jokeComponents/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Joke from './Joke';
+import jokeData from './JokeData';
 
 export default function App() {
+    // Another way to write below function is 
+
+    // const jokeContainer =jokeData.map(function(props) {
+    //     return (
+    //         <div>
+    //         <Joke key={props.id} question={props.question} punchLine={props.punchLine} />
+    //         <hr />
+    //     </div>
+    //     )
+    // })
+
+    const jokeContainer = jokeData.map((props) => (
+        <div>
+            <Joke key={props.id} question={props.question} punchLine={props.punchLine} />
+            <hr />
+        </div>
+    ))
+    console.log(jokeContainer)
     return (
         <div>
-            <Joke 
-                question="What is your name?" 
-                punchLine="something that you should know" 
-            />
-            <hr/>
-            <Joke 
-                
-                punchLine="something that you should know" 
-            />
+            {jokeContainer}
         </div>
     )
 
---
+++ JavaScript/JS_React_Project/todo-app/src/jokeComponents/JokeData.js
@@ -0,0 +1,28 @@
+const jokeData = [
+    {
+        id: 1,
+        punchLine: "First punch line"
+    },
+    {
+        id: 2,
+        question: "What are the best thing about Switzerland?",
+        punchLine: "I don't know, but the flag is a big plus!"
+    },
+    {
+        id: 3,
+        question: "Did you hear about mathematician who's afraid of negative numbers?",
+        punchLine: "He'll stop at nothing to avoid them"
+    },
+    {
+        id: 4,
+        question: "Hear about new restaurant called Karma?",
+        punchLine: "There's no menu: You get what you deserve."
+    },
+    {
+        id: 5,
+        question: "Did you hear about the claustraphobic astrounaut?",
+        punchLine: "He just need a little space."
+    }
+];
+
+export default jokeData;
