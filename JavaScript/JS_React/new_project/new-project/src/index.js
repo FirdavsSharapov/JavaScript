c937ad649c55db4102b312e4f7c51446c3f3f187
Fred Sharapov <f.sharapov@yahoo.com>
2021-04-11 13:10:58 -0400

    new Js Project

--- JavaScript/JS_React/new_project/new-project/src/index.js
+++ JavaScript/JS_React/new_project/new-project/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+function MyApp(){
+  
+}
 
 ReactDOM.render(
   <React.StrictMode>
