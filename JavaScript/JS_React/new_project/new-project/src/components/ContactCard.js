802f8b90821b4c46336ebdd97b22bf2480fbf59b
Fred Sharapov <f.sharapov@yahoo.com>
2021-05-30 18:52:05 -0400

    1/4 of job

---
+++ JavaScript/JS_React/new_project/new-project/src/components/ContactCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ContactCard() {
+    return (
+        <div className='contact-card'>
+            <img alt="" src="http://placekittent.com/300/200" />
+            <h3>Mr. Whiskerson</h3>
+            <p>Phone: 888-888-8888</p>
+            <p>Email: some@some.com</p>
+        </div>
+    );
+}
--- JavaScript/JS_React/new_project/new-project/src/index.js
+++ JavaScript/JS_React/new_project/new-project/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import MainContent from './components/MainContent';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainContent />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
