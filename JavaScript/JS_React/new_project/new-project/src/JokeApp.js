ab71765c7804199e80b1ce118a4460810b483606
Fred Sharapov <f.sharapov@yahoo.com>
2021-09-13 13:25:51 -0400

    React props

---
+++ JavaScript/JS_React/new_project/new-project/src/JokeApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Joke from './components/Joke';
+
+export default function JokeApp() {
+    return (
+        <Joke question='how to get into the library?' answer="You gotta ask librarian" />
+    )
+}
