a21e4de822a646d624e71e75613bbfb06ec7a935
Fred Sharapov <f.sharapov@yahoo.com>
2021-12-01 15:40:24 -0500

    big o finished course

--- JavaScript/JS_React/js_data_course/src/components/big_o.js
+++ JavaScript/JS_React/js_data_course/src/components/big_o.js
@@ -2,6 +2,17 @@ import React from "react";
 export default function BigO() {
     const nemo = ['Nemo', "Aza", "Mona", "Nemo2", "Mat"];
     const large_arr = new Array(100).fill('Nemo');
+
+    // Log all pair in a array
+    const boxes = [1, 2, 3, 4, 5];
+    for (let i = 0; i < boxes.length; i++) {
+        for (let j = 0; j < boxes.length; j++) {
+            console.log('pairs: ', boxes[i], boxes[j])
+        }
+    }
+
+    // three ways of the loop
+    // first
     function findNemo(array) {
         let t0 = performance.now();
         for (let i = 0; i < array.length; i++) {
@@ -16,6 +27,28 @@ export default function BigO() {
         console.log('performance time: ', t1 - t0, ' ms');
     }
     findNemo(large_arr)
+
+    //second ES6 version
+    const findNemo2 = nemo => {
+        nemo.forEach(element => {
+            if (element === 'Nemo') {
+                console.log('Found Nemo');
+            }
+        });
+    }
+    findNemo2(nemo);
+
+    //third
+    const findNemo3 = nemo => {
+        for(let element of nemo){
+            if (element === 'Nemo') {
+                console.log('Found Nemo');
+            }
+        }
+    }
+    findNemo3(nemo);
+
+
     return (
         <div></div>
     )
