9fef1af30ce3e7e0dc3b85969d483890d92909f3
Fred Sharapov <f.sharapov@yahoo.com>
2021-12-23 20:58:18 -0500

    binarytree

---
+++ JavaScript/JS_React/js_data_course/src/components/BinaryTree.js
@@ -0,0 +1,30 @@
+class Node{
+    constructor(value){
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinaryTreeSearch{
+    constructor(){
+        this.root = null;
+    }
+
+    insert(value){
+// First we need to understand in what side of the tree it will go
+// 
+    }
+
+    lookup(value){
+
+    }
+
+    remove(value){
+
+    }
+}
+
+export default function BinaryTree(){
+    return <div></div>;
+}
