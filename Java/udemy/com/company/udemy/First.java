c3f24b4b5b3c91bd3a8707e812bc5640e3da1f7e
Fred Sharapov <f.sharapov@yahoo.com>
2022-04-07 10:23:58 -0400

    Changes on workplace

--- Java/udemy/com/company/udemy/First.java
+++ Java/udemy/com/company/udemy/First.java
@@ -3,5 +3,6 @@ package com.company.udemy;
 public class First {
     public static void main (String args[]){
         System.out.println("Hello world");
+        System.out.print(args[0]);
     }
 }
