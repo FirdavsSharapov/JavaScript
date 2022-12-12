ee8c6e7a06223f42cb7333e380b4ee8ad825ad35
Fred Sharapov <f.sharapov@yahoo.com>
2022-12-11 22:16:13 -0500

    changes

--- JavaScript/JS_React/todo_project/todo-app/src/components/ToDo.js
+++ JavaScript/JS_React/todo_project/todo-app/src/components/ToDo.js
@@ -16,21 +16,21 @@ class ToDo extends React.Component {
 
     checkOnChange(id) {
         console.log('changed', id)
-    }
+    };
 
     render() {
         const todoItems = this.state.todos.map(item =>
             <ToDoItem 
                 key={item.id} 
                 item={item} 
-                checkOnChange={this.checkOnChange(item.id)} />)
+                checkOnChange={this.checkOnChange(item.id)} />);
 
         return (
             <div className="todo-list">
                 {todoItems}
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
 export default ToDo;
